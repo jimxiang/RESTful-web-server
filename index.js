fa7fc6af4bc0dcea4455b1dd7874132e4f8f7bf2
jimxiang <jxhzr94151@gmail.com>
2016-03-20 22:32:02 +0800

    full function

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var server = http.createServer(function(req, res) {
 			req.setEncoding('utf8');
 			req.on('data', function(chunk) {
 				item += chunk;
-				console.log(url.parse(req.url));
 			});
 			req.on('end', function() {
 				console.log(item);
@@ -19,10 +18,54 @@ var server = http.createServer(function(req, res) {
 			break;
 
 		case 'GET':
-			items.forEach(function(item, i) {
-				res.write(i + ') ' + item + '\n');  
+			var body = items.map(function(item, i) {
+				return (i + ')' + item);
+			}).join('\n');
+			res.setHeader('Content-Length', Buffer.byteLength(body));
+			res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
+			res.end(body);
+			// items.forEach(function(item, i) {
+			// 	res.write(i + ') ' + item + '\n');  
+			// });
+			// res.end();
+			break;
+
+		case 'DELETE':
+			var path = url.parse(req.url).pathname;
+			var i = parseInt(path.slice(1), 10);
+			if (isNaN(i)) {
+				res.statusCode = 400;
+				res.end('Invalid item id!');
+			} else if(!items[i]) {
+				res.statusCode = 404;
+				res.end('Item not found!');
+			} else {
+				items.splice(i, 1);
+				res.end('OK\n');
+			}
+			break;
+
+		case 'PUT':
+			var path = url.parse(req.url).pathname;
+			var i =parseInt(path.slice(1), 10);
+			var item = '';
+			req.setEncoding = 'utf8';
+			req.on("data", function(chunk) {
+				item += chunk;
 			});
-			res.end();
+			if (isNaN(i)) {
+				res.statusCode = 400;
+				res.end('Invalid item id!');
+			} else if(!items[i]) {
+				res.statusCode = 404;
+				res.end('Item not found!');
+			} else {
+				items[i] = '';
+				req.on('end', function() {
+					items[i] = item;
+					res.end('OK\n');
+				})
+			}
 			break;
  	}
 	
